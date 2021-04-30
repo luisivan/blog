26ce29c4dd813345842f4e7551ab3bf6536645fb
Luis Cuende <me@luisivan.net>
2021-04-30 15:42:31 +0100

    Added Gatsby Twitter plugin to render tweets

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,7 +119,8 @@ module.exports = {
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
-          'gatsby-remark-external-links'
+          'gatsby-remark-external-links',
+          'gatsby-plugin-twitter',
         ]
       }
     },
